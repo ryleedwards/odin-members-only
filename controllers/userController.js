f085e2ac91ac474e367b2326571cad53a37a5e15
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-04-23 14:39:34 -0500

    Add user_create_post for storing new user

---
+++ controllers/userController.js
@@ -0,0 +1,20 @@
+const User = require('../models/user');
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+exports.user_create_post = asyncHandler(async (req, res, next) => {
+  // bcrypt hash
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) {
+      return next(err);
+    } else {
+      // Create user object
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      res.redirect('/');
+    }
+  });
+});
