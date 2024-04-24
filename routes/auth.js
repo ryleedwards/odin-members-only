b71042ea2e97b0fa353e6889b3e56c791d2d89e4
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-04-24 09:51:54 -0500

    Add sign-up route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const user_controller = require('../controllers/userController');
 
 router.get('/login', function (req, res, next) {
   res.render('login');
 });
+
+router.get('/sign-up', user_controller.user_create_get);
+router.post('/sign-up', user_controller.user_create_post);
 
 module.exports = router;
